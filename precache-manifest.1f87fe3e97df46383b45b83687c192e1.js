a89adade69080317766450bd6e57bcae0faeb49a
Olsi Qose <olsiqose@gmail.com>
2018-10-04 19:41:52 +0200

    Updates

---
+++ precache-manifest.1f87fe3e97df46383b45b83687c192e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-fizzbuzz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed017178a89f4750bbd5",
+    "url": "/react-fizzbuzz/static/js/runtime~main.ed017178.js"
+  },
+  {
+    "revision": "818ca5dbed981a59d04a",
+    "url": "/react-fizzbuzz/static/js/main.818ca5db.chunk.js"
+  },
+  {
+    "revision": "cee5f3f7d0042024d99f",
+    "url": "/react-fizzbuzz/static/js/1.cee5f3f7.chunk.js"
+  },
+  {
+    "revision": "818ca5dbed981a59d04a",
+    "url": "/react-fizzbuzz/static/css/main.8e38a345.chunk.css"
+  },
+  {
+    "revision": "cee5f3f7d0042024d99f",
+    "url": "/react-fizzbuzz/static/css/1.321161e1.chunk.css"
+  },
+  {
+    "revision": "40c1723547bd89e986f66c4db0049e6e",
+    "url": "/react-fizzbuzz/index.html"
+  }
+];
